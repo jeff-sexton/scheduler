278b54e6393e3be839ec784fcf0e87b089a91faa
Jeff Sexton <sexton.jeffp@gmail.com>
2020-04-13 16:52:35 -0700

    Completed InterviewerListItem component

---
+++ src/components/InterviewerList/InterviewerListItem/InterviewerListItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import './InterviewerListItem.scss'
+
+
+
+const InterviewerListItem = ({name, avatar, selected, setInterviewer}) => {
+
+  const interviewerClass = classNames({
+    'interviewers__item': true,
+    'interviewers__item--selected': selected,
+  });
+
+  return (
+    <li className={interviewerClass} onClick={setInterviewer}>
+      <img
+        className="interviewers__item-image"
+        src={avatar}
+        alt={name}
+      />
+      {selected && name}
+    </li>
+  )
+}
+
+export default InterviewerListItem;
---
+++ src/components/InterviewerList/InterviewerListItem/InterviewerListItem.story.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+
+import InterviewerListItem from './InterviewerListItem'
+
+
+const interviewer = {
+  id: 1,
+  name: "Sylvia Palmer",
+  avatar: "https://i.imgur.com/LpaY82x.png"
+};
+
+storiesOf("InterviewerListItem", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Unselected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+    />
+  ))
+  .add("Selected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected
+    />
+  ))
+  .add("Clickable", () => (
+    <InterviewerListItem
+    id={interviewer.id}
+    name={interviewer.name}
+    avatar={interviewer.avatar}
+    setInterviewer={event => action("setInterviewer")(interviewer.id)}
+  />
+  ));
