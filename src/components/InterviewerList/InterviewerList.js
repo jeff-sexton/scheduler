9252f73b37fa097b0355e68cabe602137dde1ad7
Jeff Sexton <sexton.jeffp@gmail.com>
2020-04-14 12:54:46 -0700

    Moved InterviewerListItem folder into components root

--- src/components/InterviewerList/InterviewerList.js
+++ src/components/InterviewerList/InterviewerList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './InterviewerList.scss';
 
-import InterviewerListItem from './InterviewerListItem';
+import InterviewerListItem from '../InterviewerListItem';
 
 
 
--- stories/index.js
+++ stories/index.js
@@ -6,7 +6,7 @@ import '../src/components/Button/Button.stories';
 import '../src/components/DayListItem/DayListItem.stories';
 import '../src/components/DayList/DayList.stories';
 
-import '../src/components/InterviewerList/InterviewerListItem/InterviewerListItem.stories';
+import '../src/components/InterviewerListItem/InterviewerListItem.stories';
 import '../src/components/InterviewerList/InterviewerList.stories';
 
 import '../src/components/Appointment/Appointment.stories';
