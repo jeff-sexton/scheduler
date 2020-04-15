4b06f4c14f6041ead3efc901386b28573d5febda
Jeff Sexton <sexton.jeffp@gmail.com>
2020-04-14 20:12:18 -0700

    Completed getAppointmentsForDay selector

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,10 +1,12 @@
 
 
-const getAppointmentsForDay = (state, day) => {
-  const appointments = [];
-
-
-  return appointments;
+const getAppointmentsForDay = (state, targetDay) => {
+  for (const day of state.days) {
+    if (day.name === targetDay) {
+      return day.appointments.map(appointment => state.appointments[appointment]);
+    }
+  }
+  return [];
 };
 
 export { getAppointmentsForDay };
