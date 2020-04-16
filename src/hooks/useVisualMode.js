6eba6b1efb85c7fe9df0380bfbc346e3f7b91c92
Jeff Sexton <sexton.jeffp@gmail.com>
2020-04-16 12:34:03 -0700

    Refactored useVisualMode to use prev syntax

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,32 +1,69 @@
 import { useState } from 'react';
 
+// const useVisualMode = (initialMode) => {
+//   const [ mode, setMode ] = useState(initialMode);
+//   const [ history, setHistory ] = useState([initialMode])
+
+//   const transition = (newMode, replace = false) => {
+//     setMode(newMode);
+
+//     const newHistory = [ ...history ];
+//     if (replace) {
+//       newHistory[newHistory.length - 1] = newMode;
+//     } else {
+//       newHistory.push(newMode);
+//     }
+//     setHistory(newHistory);
+//   };
+  
+//   const back = () => {
+//     let newHistory = [ ...history ];
+//     if (history.length > 1) {
+//       newHistory = history.slice(0, history.length - 1)
+//       setHistory( newHistory );
+//     }
+
+//     setMode(newHistory[newHistory.length - 1]);
+//   };
+  
+//   return { mode, transition, back };
+// };
+
+// export default useVisualMode;
+
+
+
+
+// setHistory(prev => ([...prev, mode])) ?
+
 const useVisualMode = (initialMode) => {
   const [ mode, setMode ] = useState(initialMode);
   const [ history, setHistory ] = useState([initialMode])
 
-  const transition = (newMode, replace = false) => {
-    setMode(newMode);
-
-    const newHistory = [ ...history ];
+  const transition = (mode, replace = false) => {
     if (replace) {
-      newHistory[newHistory.length - 1] = newMode;
+      setHistory(prev => ([ ...prev.slice(0, prev.length - 1), mode ]));
     } else {
-      newHistory.push(newMode);
+      setHistory(prev => ([...prev, mode]));
     }
-    setHistory(newHistory);
+
+    setMode(mode);
   };
   
   const back = () => {
-    let newHistory = [ ...history ];
-    if (history.length > 1) {
-      newHistory = history.slice(0, history.length - 1)
-      setHistory( newHistory );
-    }
+    setHistory(prev => {
+      let newHistory = [...prev];
 
-    setMode(newHistory[newHistory.length - 1]);
+      if (newHistory.length > 1) {
+        newHistory = newHistory.slice(0, newHistory.length - 1);
+      }
+
+      setMode(newHistory[newHistory.length - 1]);
+      return newHistory;
+    });
   };
   
-  return { mode, transition, back };
+  return {mode, transition, back};
 };
 
 export default useVisualMode;
