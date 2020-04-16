fb8d4563e2aa5a4475a4402287d9d50a6533c5b6
Jeff Sexton <sexton.jeffp@gmail.com>
2020-04-15 17:13:52 -0700

    implemented Visual mode hook for show, empty and create

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 
 import './styles.scss';
 
+import useVisualMode from '../../hooks/useVisualMode';
+
 import Header from './header';
 import Show from './Show';
 import Empty from './Empty';
+import Form from './Form';
 
 const Appointment = ({id, time, interview}) => {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = 'CREATE';
+
+  const { mode, transition, back } = useVisualMode( interview ? SHOW : EMPTY );
+
 
   return (
     <article className="appointment">
       <Header time={time}/>
-      {interview ? <Show {...interview}/> : <Empty />}
+      {mode === SHOW && <Show {...interview}/>}
+      {mode === EMPTY && <Empty onAdd={() => {transition(CREATE)}}/>}
+      {mode === CREATE && <Form interviewers={[]} onCancel={() => {back()}} />}
     </article>
   )
 };
