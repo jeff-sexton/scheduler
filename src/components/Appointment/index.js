ff67f9ed22bc5355da8e4e61825291ae1eba4558
Jeff Sexton <sexton.jeffp@gmail.com>
2020-04-15 19:40:58 -0700

    Added Display of saving Status component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -8,15 +8,18 @@ import Header from './header';
 import Show from './Show';
 import Empty from './Empty';
 import Form from './Form';
+import Status from './Status';
 
 const Appointment = ({id, time, interview, interviewers, bookInterview}) => {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = 'CREATE';
+  const SAVING = 'SAVING';
 
   const { mode, transition, back } = useVisualMode( interview ? SHOW : EMPTY );
 
   const save = (name, interviewer) => {
+    transition(SAVING);
     const interview = {
       student: name,
       interviewer
@@ -35,6 +38,7 @@ const Appointment = ({id, time, interview, interviewers, bookInterview}) => {
       {mode === SHOW && <Show {...interview}/>}
       {mode === EMPTY && <Empty onAdd={() => {transition(CREATE)}}/>}
       {mode === CREATE && <Form interviewers={interviewers} onSave={save} onCancel={() => {back()}} />}
+      {mode === SAVING && <Status message='Saving' />}
     </article>
   )
 };
