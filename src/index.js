4458ec08f1bcd450376bb44655da091ef1ae8c2b
Jeff Sexton <sexton.jeffp@gmail.com>
2020-04-22 16:23:14 -0700

    Setup for netify deployment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 /* istanbul ignore file */
 
+import axios from "axios";
+
+// Setup base url based on env variables
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
+
 import React from "react";
 import ReactDOM from "react-dom";
 
