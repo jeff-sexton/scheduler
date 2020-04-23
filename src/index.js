56e0b8d37acaf49a2d09e571196effe09ee8ec33
Jeff Sexton <sexton.jeffp@gmail.com>
2020-04-22 18:03:08 -0700

    reordered imports to fix build error

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 /* istanbul ignore file */
+import React from "react";
+import ReactDOM from "react-dom";
 
 import axios from "axios";
+import "index.scss";
+
+import Application from "components/Application";
 
 // Setup base url based on env variables
 if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
 }
 
-import React from "react";
-import ReactDOM from "react-dom";
 
-import "index.scss";
 
-import Application from "components/Application";
 
 ReactDOM.render(<Application />, document.getElementById("root"));
